ffb5f61c630005435972bcd4a23043b5232811b5
Jakub Ježek <jezekj22@spstrutnov.cz>
2025-05-08 13:09:08 +0200

    založení C# projektu + gitignore

---
+++ NotesAppAspNet/NotesAppAspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NotesAppAspNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
