89dac10bcdc847310368198673d157129ccccead
Jakub Ježek <jezekj22@spstrutnov.cz>
2025-05-22 19:06:32 +0200

    příprava lokální databáze

---
+++ c#-mat-zad/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesAppAspNet.Models;
+
+namespace NotesAppAspNet.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ c#-mat-zad/Models/Note.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NotesAppAspNet.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public bool Important { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
